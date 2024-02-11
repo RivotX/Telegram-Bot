f04aa3d73fd516f6d8ee85f6f708397d42624f39
Miguecaritatriste <114624729+Miguecaritatriste@users.noreply.github.com>
2024-02-11 17:40:05 +0100

    fixed openai talking while bot was waiting for user input on /weatehr or /ytmp3

--- src/bot.js
+++ src/bot.js
@@ -3,13 +3,18 @@ const { Telegraf } = require("telegraf");
 const { Markup } = require("telegraf");
 const commands = require("./commands/commands");
 
+const LocalSession = require('telegraf-session-local');
+
+const localSession = new LocalSession({ database: 'session_db.json' });
 
 const bot = new Telegraf(process.env.TELEGRAM_API_TOKEN);
+bot.use(localSession.middleware());
+
 
 bot.start((ctx) => {
   ctx.reply(
     "💗Hi, I'm RivotX'bot, im here to assist you!. You can chat with me or try /help to see the aviable commands 💗",
-    Markup.keyboard([["/help", "/weather"], ["/waifu"]])
+    Markup.keyboard([["/help", "/weather"], ["/waifu", "/YTmp3"]])
       .oneTime() //una vez que se usa la primera vez, desaparece
       .resize() //para que se ajuste al tamaño del teclado
   );
@@ -23,13 +28,12 @@ bot.command("looser", (ctx) => {
   commands.looser(ctx);
 });
 
+
 bot.command("weather", (ctx) => {
-  ctx.reply("Select a place to get the weather🤖").then(() => {
-    bot.hears(/.*/, (ctx) => {
-      commands.weather(ctx, ctx.message.text);
-    });
-  });
+  ctx.reply("Select a place to get the weather🤖")
+  ctx.session.awaitingWeather = true;
 });
+
 
 bot.command("waifu", (ctx) => {
   commands.waifu(ctx);
@@ -42,11 +46,22 @@ bot.action(["maid", "oppai", "selfies", "uniform"], (ctx) => {
 
 
 bot.command("YTmp3", (ctx) => {
-  ctx.reply("Send the YouTube link to convert the video to MP3 \nit must be an absolute url").then(() => {
-    bot.hears(/.*/, (ctx) => {
-      commands.YTmp3(ctx, ctx.message.text);
-    });
-  });
+  ctx.reply("Send the YouTube link to convert the video to MP3 \nit must be an absolute url");
+  ctx.session.awaitingYTmp3 = true;
+});
+
+// Si está esperando a que el usuario pase informacion para un comando, se ejecuta el comando en vez de openai
+bot.on('message', (ctx) => {
+  if (ctx.session.awaitingYTmp3) {
+    commands.YTmp3(ctx, ctx.message.text);
+    ctx.session.awaitingYTmp3 = false;
+  } else if (ctx.session.awaitingWeather) {
+    commands.weather(ctx, ctx.message.text);
+    ctx.session.awaitingWeather = false;
+  }
+  else {
+    commands.chatWithOpenAI(ctx);
+  }
 });
 
 bot.on('message', commands.chatWithOpenAI);
